c89d2fab9a19b9b41328eb84ffa88b527186773b
maxjeanty <maxjeanty@gmail.com>
2023-11-07 16:32:18 -0600

    finished HTm and CSS starting JS decaring variable

---
+++ script.js
@@ -0,0 +1,4 @@
+const form = document.getElementById('form')
+const search = document.getElementById('search')
+const result = document.getElementById('result')
+const more = document.getElementById('more')
