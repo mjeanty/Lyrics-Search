29a82407cd9dd06e9a81d1f19c2a147999255a74
maxjeanty <maxjeanty@gmail.com>
2023-11-07 22:40:22 -0600

    added fetch and asynch funtonality and show songs funtonality

--- script.js
+++ script.js
@@ -7,12 +7,38 @@ const apiURL = 'https://api.lyrics.ovh';
 
 
 // searchby song or artists
-function searchSongs(term) {
-    fetch(`${apiURL}/suggest/${term}`)
-    .then(res => res.json())
-    .then(data => console.log(data))
+async function searchSongs(term) {
+
+    const res = await fetch(`${apiURL}/suggest/${term}`)
+    const data = await res.json()
+    showData(data)
 }
 
+///show sng and artist in DOM
+function showData(data){
+    let output = '';
+
+    data.data.forEach(song => {
+        output += ` 
+            <li>
+                <span><strong>${song.artist.name}</strong> - 
+                ${song.title}</span>
+                <button class="btn" data-artist="${song.artist.name}" data-songtitle="${song.title}">Get Lyrics</button>
+            </li>
+        
+        
+        `;
+        
+
+    })
+    result.innerHTML =`
+        <ul class="songs">
+        ${output}
+        </ul>
+    
+    `;
+
+}
 
 // event Listener
 
